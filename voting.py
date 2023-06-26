f228f84dcd644fd0e226a78b32f01353258ebc13
sifumaster <phivos93@yahoo.com>
2023-06-26 14:14:34 +0300

    ADD soft voting

--- voting.py
+++ voting.py
@@ -2,11 +2,12 @@ import csv
 
 def vote(mode):
     votes = {}
-    with open("./results.csv", 'r') as file:
-        csvreader = csv.reader(file)
-        next(csvreader)
 
-        if mode == "majority":
+    if mode == "majority":
+        with open("./results.csv", 'r') as file:
+            csvreader = csv.reader(file)
+            next(csvreader)
+    
             for row in csvreader:
                 prediction = row[2]
                 truth = row[3]
@@ -31,14 +32,63 @@ def vote(mode):
                         votes[study] = [-1, 0, truth]
             studies = list(votes.keys())
             values = list(votes.values())
-            
+
             header_row = ['Study', 'Votes', 'Prediction', 'Truth']
             value_rows = [[studies[i]] + values[i] for i in range(len(studies))]
-            #print(value_rows)
+            # print(value_rows)
 
-            
-            with open('majority_voting.csv', 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(header_row)
-                writer.writerows(value_rows)
+
+        with open('majority_voting.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header_row)
+            writer.writerows(value_rows)
+    elif mode == "soft":
+        with open("./results_probs.csv", 'r') as file:
+            csvreader = csv.reader(file)
+            next(csvreader)
+
+            for row in csvreader:
+                prediction_probs = (row[2]).strip('][').split(', ')
+                prediction_probs = [float(prediction_probs[0]), float(prediction_probs[1])]
+                
+                print(prediction_probs)
+                print(type(prediction_probs))
+                
+                truth = row[2]
+
+                path = row[1]
+                path = path.split('/')
+                study = path[2] + '-' + path[3]
+                
+                if study in votes:
+                    votes[study] =[ [votes[study][0][0] + prediction_probs[0], votes[study][0][1] + prediction_probs[1]], None, truth, votes[study][3]+1 ]   
+                else:
+                    votes[study] = [ prediction_probs, None, truth, 1]
+                                        
+            for study in votes:
+
+                if votes[study][0][0] >= votes[study][0][1]:
+                    votes[study] = [  [votes[study][0][0]/votes[study][3], votes[study][0][1]/votes[study][3] ] , 0, truth, votes[study][3] ]
+                else:
+                    votes[study] = [  [votes[study][0][0]/votes[study][3], votes[study][0][1]/votes[study][3] ] , 1, truth, votes[study][3] ]
+
+        
+        with open('soft_voting.csv', 'w', newline='') as file:
+            header_row = ['Study', 'Votes_Prob', 'Prediction', 'Truth', '#Images']
+                          
+            studies = list(votes.keys())
+            values = list(votes.values())
+
+            header_row = ['Study', 'Votes', 'Prediction', 'Truth']
+            value_rows = [[studies[i]] + values[i] for i in range(len(studies))]
+
+            writer = csv.writer(file)
+            writer.writerow(header_row)
+            writer.writerows(value_rows)
+        with open('soft_voting.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header_row)
+            writer.writerows(value_rows)
+
+
                 
